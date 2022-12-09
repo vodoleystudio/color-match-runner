aff291db1c7b1c4e8a7bb4b789f90e1b7a6ef271
jnjhi <deniskub2007@gmail.com>
2022-12-09 10:55:29 +0200

    ref:fix name space

--- Assets/GameCore/Services/GamePlayProgresServes.cs
+++ Assets/GameCore/Services/GamePlayProgresServes.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace GameCore.Services
 {
-    public class GamePlayProgresServes
+    public class GamePlayProgresServes :IGamePlayProgresService
     {
         // Start is called before the first frame update
         void Start()
--- Assets/GameCore/Services/IGamePlayProgresService.cs
+++ Assets/GameCore/Services/IGamePlayProgresService.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class IGamePlayProgresService : MonoBehaviour
+namespace GameCore.Services
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IGamePlayProgresService
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
+
