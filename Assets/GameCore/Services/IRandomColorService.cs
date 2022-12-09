a04388919f35af1993ba74e4e4332ccad44f062e
jnjhi <deniskub2007@gmail.com>
2022-12-09 10:47:48 +0200

    ref: fix comments

--- Assets/GameCore/Services/IRandomColorService.cs
+++ Assets/GameCore/Services/IRandomColorService.cs
@@ -5,6 +5,6 @@ namespace GameCore.Services
     public interface IRandomColorService
     {
         Color GetRandomColor();
-        Color GetSimilarColor(Color color, float TheDifrentBtwThwColors);
+        Color GetSimilarColor(Color color , float diffrenceThreshold);
     }
 }
