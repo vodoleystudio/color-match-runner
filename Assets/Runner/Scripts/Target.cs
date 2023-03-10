6191b0759c1f75799cc36448b05fdea9ceb3bfe1
vodoleystudio <vodoleystudio@gmail.com>
2023-03-10 20:53:13 +0200

    fix: particles and dragons outlines and color

--- Assets/Runner/Scripts/Target.cs
+++ Assets/Runner/Scripts/Target.cs
@@ -10,25 +10,14 @@ namespace HyperCasual.Runner
     /// </summary>
     public class Target : TargetBase
     {
-        protected override void OnSetBaseColor(Color baseColor)
-        {
-            var meshRenderers = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
-            if (meshRenderers != null)
-            {
-                for (int i = 0; i < meshRenderers.Length; i++)
-                {
-                    var meshRenderer = meshRenderers[i];
+        [SerializeField]
+        private SkinnedMeshRenderer m_SkinnedMeshRenderer;
 
-                    if (meshRenderer != null && meshRenderer.sharedMaterial != null)
-                    {
-                        Material material = new Material(meshRenderer.sharedMaterial);
-                        material.SetColor("_1st_ShadeColor", baseColor);
-                        material.SetColor("_BaseColor", baseColor);
-                        material.color = baseColor;
-                        meshRenderer.sharedMaterial = material;
-                    }
-                }
-            }
+        protected override void OnSetBaseColor(Color color)
+        {
+            m_SkinnedMeshRenderer.material.color = color;
+            m_SkinnedMeshRenderer.material.SetColor("_1st_ShadeColor", color);
+            m_SkinnedMeshRenderer.material.SetColor("_BaseColor", color);
         }
     }
 }
